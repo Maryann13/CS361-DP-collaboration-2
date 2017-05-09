81e528ede65056755b4407c7bb29db9fa015849a
ABSLord <antonio.maksimovich@gmail.com>
2017-05-09 22:35:14 +0300

    add basic classes and implenemnt some of them

---
+++ Core/Formula.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public abstract class Component
+    {
+        public abstract void AddOperation(Operation operation);
+        public List<Operation> operations { get; set; }
+    }
+
+    //наша главная формула
+    public class Formula : Component
+    {
+        //инициализация формулы константой
+        public void InitWithConst(string c)
+        {
+            if (operations.Count == 0)
+            {
+                Operation init = new Operation();
+                init.consts.Add(c);
+                init.typename = "const";
+                operations.Add(init);
+            }
+        }
+        //инициализация формулы переменной
+        public void InitWithVar(string var)
+        {
+            if (operations.Count == 0)
+            {
+                Operation init = new Operation();
+                init.consts.Add(var);
+                init.typename = "var";
+                operations.Add(init);
+            }
+        }
+        public override void AddOperation(Operation operation)
+        {
+            //не делать ничего, вся работа будет в AddOperation у декоратора конкретной операции
+        }
+
+        public string Calculate()
+        {
+            //todo
+            //для каждой из операций будет вызываться функция Calculate из соответствующего декоратора
+            //кроме первой(которая задается инициализаций const или var)
+            return "";
+        }
+    }
+}
---
+++ Core/Operation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    //структура для хранения вместе переменных и констант у конкретной операции
+    public struct Operation
+    {
+        public string typename;
+        public List<string> variables { get; set; }
+        public List<string> consts { get; set; }
+    }
+}

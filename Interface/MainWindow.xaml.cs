e9694535e55c0d27ec5afb159553ec54f6b7a348
ABSLord <antonio.maksimovich@gmail.com>
2017-05-21 01:31:33 +0300

    some changes

--- Interface/MainWindow.xaml.cs
+++ Interface/MainWindow.xaml.cs
@@ -20,6 +20,23 @@ namespace Interface
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Dictionary<string, string> var_dict = new Dictionary<string, string>();
+
+        public bool IsValidVar(string var)
+        {
+            if (var == null || var == "")
+                return false;
+            if (var_dict != null)
+            {
+                foreach (string key in var_dict.Keys)
+                    if (var == key)
+                        return false;
+            }
+            if (var.Contains(" ") || var.Length > 3)
+                return false;
+            return true;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,6 +48,45 @@ namespace Interface
         }
 
         private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        //кнопки для работы со списком переменных
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            string var = new_var.Text;
+            if(!IsValidVar(var))
+            {
+                MessageBox.Show("Not valid variable name!");
+                return;
+            }
+            var_dict.Add(var, "");
+            variables.Items.Add(var); 
+        }
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            string elem = (string)variables.SelectedItem;
+            if (elem != null)
+            {
+                var_dict.Remove(elem);
+                variables.Items.Remove(elem);
+            }
+            else
+            {
+                elem = new_var.Text;
+                if (var_dict.ContainsKey(elem))
+                {
+                    var_dict.Remove(elem);
+                    variables.Items.Remove(elem);
+                }
+
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
         {
 
         }
